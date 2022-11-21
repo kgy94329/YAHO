b1f4d3f107f55961ed8b414b5bd7815d45531671
Cookirumble <rndud329@gmail.com>
2022-11-21 17:16:43 +0900

    second

--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
+'''
+File: main.py
+Author: Guyong Kwon, Hangyul Lee
+Date: 2022-11-21
+Note:
+    서버로부터 데이터를 받아 AI처리를 수행하는 모듈
+    AI기능
+    1. 회의 대화 내용 데이터로부터 키워드 추출
+    2. 추출된 키워드를 활용하여 추출요약 진행
+    3. 입력받은 유저 사진 이미지가 정면인지 측면인지 체크
+    4. 1분마다 한 장의 사진을 입력받고 유저가 있는지 없는지 체크
+
+    그 외 기능
+    1. 회의 대화 참여율 그래프 작성
+'''
 from flask import Flask , Response, request
 from collections import Counter
 import base64
@@ -19,7 +34,6 @@ app = Flask(__name__)
 
 
 model_name = 'alaggung/bart-r3f'
-# model_name = 'ainize/kobart-news'
 
 summarizer = summarize.summarize(model_name , model_name )
 
@@ -40,16 +54,6 @@ def process(data):
     # 요약문 생성
     summary = summarizer.get_summary(keyword, doc)
     return img, rec_script, keyword, summary
-# @app.route('/send', methods=['GET'])
-# def send_result():
-#     global summary, rec_script, keyword, img, date, pe_count
-    
-#     print('response를 보냅니다.')
-#     # response 생성
-#     req = {"date":date, "graph":img, "text":rec_script, "keyword":keyword, "summary":summary, "members":list(pe_count.keys())}
-#     req = jsonpickle.encode(req)
-
-#     return Response(response=req, status = 200 , mimetype='application/json')
     
 
 @app.route('/summarize', methods=['POST'])
@@ -72,19 +76,6 @@ def render_file():
 
         return Response(response=response , status = 200 , mimetype='application/json')
     return 'Got wrong request' 
-
-# @app.route('/info', methods=['POST'])
-# def counter():
-#     global member, date
-#     if request.method == 'POST':
-#         date, member = "", 0
-#         req = request.get_json()
-        
-#         member = req['count']
-#         date = req['date']
-        
-#         return "Got information of meeting"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' , port = 9090)
