e396cb44502dd4b1da9d2bdf7778fd8fac159e74
CookieCrumble <rndud329@gmail.com>
2022-11-21 10:31:10 +0000

    Third

--- src/main.py
+++ src/main.py
@@ -39,18 +39,7 @@ def process(data):
 
     # 요약문 생성
     summary = summarizer.get_summary(keyword, doc)
-    return img, rec_script, keyword, summary
-# @app.route('/send', methods=['GET'])
-# def send_result():
-#     global summary, rec_script, keyword, img, date, pe_count
-    
-#     print('response를 보냅니다.')
-#     # response 생성
-#     req = {"date":date, "graph":img, "text":rec_script, "keyword":keyword, "summary":summary, "members":list(pe_count.keys())}
-#     req = jsonpickle.encode(req)
-
-#     return Response(response=req, status = 200 , mimetype='application/json')
-    
+    return img, rec_script, keyword, summary    
 
 @app.route('/summarize', methods=['POST'])
 def render_file():
@@ -60,30 +49,19 @@ def render_file():
         data = []
         #print(f'data: {data}')
         print('리퀘스트를 받았습니다.')
-        
-        for i in req['conversation']:
-            data.append([f'{i["name"]}:{i["data"]}', i["time"]])
+        print(req)
+        for i in req['conversation'][0]:
+            data.append([f'{i["name"]}:{i["text"]}', i["time"]])
 
         img, rec_script, keyword, summary = process(data)
         print('response를 보냅니다.')
         # response 생성
-        response = {"graph":img, "data":rec_script, "keyword":keyword, "summary":summary}
+        response = {"graph":img, "record":rec_script, "keyword":keyword, "summary":summary}
         response = jsonpickle.encode(response)
 
         return Response(response=response , status = 200 , mimetype='application/json')
     return 'Got wrong request' 
 
-# @app.route('/info', methods=['POST'])
-# def counter():
-#     global member, date
-#     if request.method == 'POST':
-#         date, member = "", 0
-#         req = request.get_json()
-        
-#         member = req['count']
-#         date = req['date']
-        
-#         return "Got information of meeting"
 
 
 if __name__ == '__main__':
